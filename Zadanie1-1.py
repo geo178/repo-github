187589cb819174051e3050bc0cb55e4a3d7a273a
geo178 <62381216+geo178@users.noreply.github.com>
2020-04-06 18:36:52 +0300

    Homework Lesson 1
    Horray! Horray! Horray!

---
+++ Zadanie1-1.py
@@ -0,0 +1,16 @@
+n = 5
+b = n + 3
+print("5+3 =", b)
+
+n = '12'  #str
+v = 45 // int(n)  # int
+print(bool(v - 3))  #  bool
+
+a = 6
+print(type(a))
+a = 2.5 + a
+print(type(a), a)
+a = "eight"
+print(type(a), a)
+a = True
+print(type(a), a)
