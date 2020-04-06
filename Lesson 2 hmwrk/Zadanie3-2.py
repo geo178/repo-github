8f8bbcbe638aacbdebebc84adcff0d2fba1ccb10
geo178 <62381216+geo178@users.noreply.github.com>
2020-04-06 18:47:29 +0300

    Update Zadanie3-2.py

--- Lesson 2 hmwrk/Zadanie3-2.py
+++ Lesson 2 hmwrk/Zadanie3-2.py
@@ -4,9 +4,6 @@
 Напишите решения через list и через dict.
 '''
 
-
-
-
 # m_dict = {1: "january", 2: "february", 3: "march", 4: "april", 5: "may", 6: "june",
 #           7: "july", 8: "august", 9: "september", 10: "october", 11: "november", 12: "december"}
 
